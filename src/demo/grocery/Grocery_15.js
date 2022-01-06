112ed3854c99a0d01bcb58abb1cc32078e70fcb8
huang0308 <209410215@gms.tku.edu.tw>
2022-01-06 19:18:44 +0800

    grocery

--- src/demo/grocery/Grocery_15.js
+++ src/demo/grocery/Grocery_15.js
@@ -1,55 +1,97 @@
-import React, { useState } from 'react'
-import List_15 from './List_15';
-import './Grocery_15.css'
-
-const Grocery_15 = () => {
-
+import React, { useState, useEffect } from 'react';
+import List from './List_15';
+import Alert from './Alert_15';
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem('list')));
+  } else {
+    return [];
+  }
+};
+function Grocery_15() {
   const [name, setName] = useState('');
-  const [list,setList] = useState([]);
-  const [isEditing, setIsEditing]=useState(false);
-  const [editId,setEditId] = useState(null);
-
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      //alert
-    }else if(name && isEditing){
-
-    }else{
-      const newItem = {
-        id: new Date.getTime().toString(),
-        title:name
-      }
-      setList(...list,newItem);
+      showAlert(true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
       setName('');
-     }
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
+    } else {
+      showAlert(true, 'success', 'item added to the list');
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
   };
 
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
+  };
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id));
+  };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
+
   return (
-    <section className="section-center">
-      <form className="grocery-form" onSubmit= {handleSubmit}>
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+
         <h3>grocery bud</h3>
-        <div className="form-control">
+        <div className='form-control'>
           <input
-            type="text"
-            className="grocery"
-            placeholder="e.g. eggs"
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <button type="submit" className="submit-btn">
-            { isEditing ? 'edit' : 'submit'}
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
-      { list.length > 0 &&(
-         <div className="grocery-container">
-         <List_15 item={list}/>
-         <button className="clear-btn">clear items</button>
-     </div>
-  )}
-  </section>
-    );
-};
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
 
 export default Grocery_15;
